1f947e0ab0e91fa43a4c4206aa592cf26bc21c89
Naod Demissie <contactnaod@gmail.com>
2024-08-30 09:06:07 +0300

    removed user routes

--- app/routers/user.py
+++ app/routers/user.py
@@ -65,29 +65,29 @@ def get_user(
     return UserServices.get_user(db, current_user.user_id)
 
 
-@router.get(
-    "/get-users",
-    response_model=List[UserOutputSchema],
-    status_code=status.HTTP_200_OK,
-    description="Get a list of users with optional selection criteria.",
-)
-def get_users(
-    organization: Optional[str] = None,
-    current_user: UserOutputSchema = Depends(AuthServices.get_current_user),
-    db: Session = Depends(create_session),
-) -> List[UserOutputSchema]:
-    """
-    Get a list of users with optional selection criteria.
+# @router.get(
+#     "/get-users",
+#     response_model=List[UserOutputSchema],
+#     status_code=status.HTTP_200_OK,
+#     description="Get a list of users with optional selection criteria.",
+# )
+# def get_users(
+#     organization: Optional[str] = None,
+#     current_user: UserOutputSchema = Depends(AuthServices.get_current_user),
+#     db: Session = Depends(create_session),
+# ) -> List[UserOutputSchema]:
+#     """
+#     Get a list of users with optional selection criteria.
 
-    Args:
-    - organization (str, optional): organization to filter by
-    - current_user (UserOutputSchema): Current user data
-    - db (Session): Database session
+#     Args:
+#     - organization (str, optional): organization to filter by
+#     - current_user (UserOutputSchema): Current user data
+#     - db (Session): Database session
 
-    Returns:
-    - List[UserOutputSchema]: List of users data
-    """
-    return UserServices.get_users(db, current_user, organization=organization)
+#     Returns:
+#     - List[UserOutputSchema]: List of users data
+#     """
+#     return UserServices.get_users(db, current_user, organization=organization)
 
 
 @router.get(
@@ -138,48 +138,48 @@ def update_profile(
     return UserServices.update_profile(db, current_user.user_id, update_data)
 
 
-@router.post(
-    "/update-password",
-    response_model=dict,
-    status_code=status.HTTP_200_OK,
-    description="Update the password for the current user.",
-)
-def update_password(
-    update_data: UpdatePasswordSchema,
-    db: Session = Depends(create_session),
-) -> dict:
-    """
-    Update the password for the current user.
+# @router.post(
+#     "/update-password",
+#     response_model=dict,
+#     status_code=status.HTTP_200_OK,
+#     description="Update the password for the current user.",
+# )
+# def update_password(
+#     update_data: UpdatePasswordSchema,
+#     db: Session = Depends(create_session),
+# ) -> dict:
+#     """
+#     Update the password for the current user.
 
-    Args:
-    - update_data (UpdatePasswordSchema): Update password data including current password, new password, and confirm new password
-    - db (Session): Database session
+#     Args:
+#     - update_data (UpdatePasswordSchema): Update password data including current password, new password, and confirm new password
+#     - db (Session): Database session
 
-    Returns:
-    - dict: Dictionary containing a message confirming the password update
-    """
-    return UserServices.update_password(db, update_data)
+#     Returns:
+#     - dict: Dictionary containing a message confirming the password update
+#     """
+#     return UserServices.update_password(db, update_data)
 
 
-@router.put(
-    "/request-password-reset",
-    response_model=dict,
-    description="Reset the password for the current user using an OTP.",
-)
-def request_password_reset(
-    email: EmailStr,
-    db: Session = Depends(create_session),
-) -> dict:
-    """
-    Reset the password for the current user using an OTP.
+# @router.put(
+#     "/request-password-reset",
+#     response_model=dict,
+#     description="Reset the password for the current user using an OTP.",
+# )
+# def request_password_reset(
+#     email: EmailStr,
+#     db: Session = Depends(create_session),
+# ) -> dict:
+#     """
+#     Reset the password for the current user using an OTP.
 
-    Args:
-    - reset_data (ResetPasswordSchema): Reset password data including email and OTP
-    - current_user (UserOutputSchema): Current user data
-    - db (Session): Database session
+#     Args:
+#     - reset_data (ResetPasswordSchema): Reset password data including email and OTP
+#     - current_user (UserOutputSchema): Current user data
+#     - db (Session): Database session
 
-    Returns:
-    - dict: Dictionary containing a message confirming the password reset
-    """
+#     Returns:
+#     - dict: Dictionary containing a message confirming the password reset
+#     """
 
-    return UserServices.request_password_reset(db, email)
+#     return UserServices.request_password_reset(db, email)
